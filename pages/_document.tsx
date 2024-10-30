9c8f604ff9daf4b92c677f728587dc7ae4892060
goyalsimran0503 <simran.goyal.c@vupico.com>
2024-10-30 12:21:46 +0530

    Google Ads

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,5 @@
+
+
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from 'next/script';
 
@@ -11,18 +13,15 @@ export default function Document() {
                     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
                     crossOrigin="anonymous"
                 />
-                {/* <!-- Google tag (gtag.js) --> */}
-
-
                 <title>|| Paramaarth ||</title>
             </Head>
 
             <body>
                 <Main />
                 <NextScript />
-                <script async src="https://www.googletagmanager.com/gtag/js?id=G-E7S28HQRQ9"></script>
 
-                {/* Font Awesome Script */}
+                {/* Google Analytics with lazy loading */}
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-E7S28HQRQ9" strategy="afterInteractive" />
                 <Script id="google-analytics" strategy="afterInteractive">
                     {`
                         window.dataLayer = window.dataLayer || [];
@@ -32,37 +31,39 @@ export default function Document() {
                     `}
                 </Script>
 
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8376056185309199"
-                    crossOrigin="anonymous"></script>
-                {/* <!-- Sidebar 1 --> */}
-                <ins className="adsbygoogle"
-                    style={{ display: "block" }}
-                    data-ad-client="ca-pub-8376056185309199"
-                    data-ad-slot="6931571639"
-                    data-ad-format="auto"
-                    data-full-width-responsive="true"></ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({ });
-                </script>
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8376056185309199"
-                    crossOrigin="anonymous"></script>
-                {/* <!-- Footer 1 --> */}
-                <ins className="adsbygoogle"
-                    style={{ display: "block" }}
-                    data-ad-client="ca-pub-8376056185309199"
-                    data-ad-slot="3235869207"
-                    data-ad-format="auto"
-                    data-full-width-responsive="true"></ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({ });
-                </script>
+                {/* Ads by Google */}
                 <Script
                     async
                     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8376056185309199"
                     crossOrigin="anonymous"
                     strategy="lazyOnload"
                 />
+                
+                {/* Ad - Sidebar 1 */}
+                <ins className="adsbygoogle"
+                    style={{ display: "block" }}
+                    data-ad-client="ca-pub-8376056185309199"
+                    data-ad-slot="6931571639"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true">
+                </ins>
+                <Script strategy="afterInteractive" id="sidebar-ad">
+                    {`(adsbygoogle = window.adsbygoogle || []).push({});`}
+                </Script>
 
+                {/* Ad - Footer 1 */}
+                <ins className="adsbygoogle"
+                    style={{ display: "block" }}
+                    data-ad-client="ca-pub-8376056185309199"
+                    data-ad-slot="3235869207"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true">
+                </ins>
+                <Script strategy="afterInteractive" id="footer-ad">
+                    {`(adsbygoogle = window.adsbygoogle || []).push({});`}
+                </Script>
+
+                {/* Font Awesome */}
                 <Script
                     src="https://kit.fontawesome.com/96239c21a7.js"
                     crossOrigin="anonymous"
@@ -77,7 +78,6 @@ export default function Document() {
                     strategy="lazyOnload"
                 />
             </body>
-
         </Html>
     );
 }
---
+++ pages/testing/Testing.tsx
@@ -0,0 +1,84 @@
+// import { Html, Head, Main, NextScript } from "next/document";
+// import Script from 'next/script';
+
+// export default function Document() {
+//     return (
+//         <Html style={{ backgroundColor: "#f5a95d" }}>
+//             <Head>
+//                 <link
+//                     href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+//                     rel="stylesheet"
+//                     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+//                     crossOrigin="anonymous"
+//                 />
+//                 {/* <!-- Google tag (gtag.js) --> */}
+
+
+//                 <title>|| Paramaarth ||</title>
+//             </Head>
+
+//             <body>
+//                 <Main />
+//                 <NextScript />
+//                 <script async src="https://www.googletagmanager.com/gtag/js?id=G-E7S28HQRQ9"></script>
+
+//                 {/* Font Awesome Script */}
+//                 <Script id="google-analytics" strategy="afterInteractive">
+//                     {`
+//                         window.dataLayer = window.dataLayer || [];
+//                         function gtag(){dataLayer.push(arguments);}
+//                         gtag('js', new Date());
+//                         gtag('config', 'G-E7S28HQRQ9');
+//                     `}
+//                 </Script>
+
+//                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8376056185309199"
+//                     crossOrigin="anonymous"></script>
+//                 {/* <!-- Sidebar 1 --> */}
+//                 <ins className="adsbygoogle"
+//                     style={{ display: "block" }}
+//                     data-ad-client="ca-pub-8376056185309199"
+//                     data-ad-slot="6931571639"
+//                     data-ad-format="auto"
+//                     data-full-width-responsive="true"></ins>
+//                 <script>
+//                     (adsbygoogle = window.adsbygoogle || []).push({ });
+//                 </script>
+//                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8376056185309199"
+//                     crossOrigin="anonymous"></script>
+//                 {/* <!-- Footer 1 --> */}
+//                 <ins className="adsbygoogle"
+//                     style={{ display: "block" }}
+//                     data-ad-client="ca-pub-8376056185309199"
+//                     data-ad-slot="3235869207"
+//                     data-ad-format="auto"
+//                     data-full-width-responsive="true"></ins>
+//                 <script>
+//                     (adsbygoogle = window.adsbygoogle || []).push({ });
+//                 </script>
+//                 <Script
+//                     async
+//                     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8376056185309199"
+//                     crossOrigin="anonymous"
+//                     strategy="lazyOnload"
+//                 />
+
+//                 <Script
+//                     src="https://kit.fontawesome.com/96239c21a7.js"
+//                     crossOrigin="anonymous"
+//                     strategy="lazyOnload"
+//                 />
+
+//                 {/* Bootstrap JS */}
+//                 <Script
+//                     src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+//                     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+//                     crossOrigin="anonymous"
+//                     strategy="lazyOnload"
+//                 />
+//             </body>
+
+//         </Html>
+//     );
+// }
+
